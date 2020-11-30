70c39a37c7f16388784a843be9bdfef768dbee17
DorogAD <sa_do@tut.by>
2020-11-30 18:00:43 +0300

    admin tuning

--- scoresite/remind/admin.py
+++ scoresite/remind/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AttributeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("attr_title",)}
+
+
+class GameAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("game_title",)}
+
+
+class GamerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("gamer_name",)}
+
+
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+    # prepopulated_fields = {"slug": ("gamer_name",)}
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    pass
+    # prepopulated_fields = {"slug": ("gamer_name",)}
+
+
+admin.site.register(Attribute, AttributeAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Gamer, GamerAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Score, ScoreAdmin)
